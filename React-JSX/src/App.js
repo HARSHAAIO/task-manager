15ce7fa7ba3903266cb2fcbb20821d9c943fd7e6
harshaaio <hareeshkumar5265@gmail.com>
2023-10-31 13:46:01 +0530

    react-jsx

--- React-JSX/src/App.js
+++ React-JSX/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="task-manager">
+      <div className="header">
+        <h1>Task Manager - ASHUHRASHA INNOVATIONS</h1>
+      </div>
+
+      <div className="task-view">
+        <table>
+          <tr>
+            <th>#</th>
+            <th>Task Name</th>
+            <th>Status</th>
+            <th>Deadline</th>
+          </tr>
+
+          <tr>
+            <td>1</td>
+            <td>Create a youtube video</td>
+            <td>Progress</td>
+            <td>28/10/2023</td>
+          </tr>
+
+          <tr>
+            <td>2</td>
+            <td>Create a youtube video on reactjs</td>
+            <td>Incomplete</td>
+            <td>29/10/2023</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
